9216c52842f86873411f8b7e00c0e38eb151a35b
Matt Neave <matthewneaveis@hotmail.co.uk>
2016-10-15 12:42:07 +0100

    add initial files

---
+++ app.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var request = require('request');
+
+app.use(express.static('assets'));
+app.use(bodyParser.json({limit: '20mb'}));
+app.use(bodyParser.urlencoded({extended: true, limit: '20mb'}));
+
+app.get('/', function (req, res) {
+    res.sendfile('assets/index.html');
+});
+
+app.get('/gifs', function (req, res) {
+    res.sendfile('assets/gifs.html');
+});
+
+app.get('/motion', function(req, res) {
+	console.log("Motion detected");
+	request("https://maker.ifttt.com/trigger/web_button_is_pressed/with/key/l9uHDHqqI570kbcVJ9_jFummpIxMaD069ZRJXvCHLuW", function (error, response, body) {
+		console.log('E-mail sent');
+		res.send(200);
+	});
+});
+
+var port = process.env.PORT || 3000;
+
+var server = app.listen(port, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('App listening at http://%s:%s', host, port);
+});
+
+
